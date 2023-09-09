c6f788127513ebf84708e6505c26160eed969311
Aurelain <aurelain@gmail.com>
2023-09-09 12:35:41 +0300

    intercepted the SX object

--- scripts/plugins/EsbuildEmotionPlugin.js
+++ scripts/plugins/EsbuildEmotionPlugin.js
@@ -1,6 +1,10 @@
+import fs from 'fs';
+
 // =====================================================================================================================
 //  D E C L A R A T I O N S
 // =====================================================================================================================
+const SX_PATTERN = /const SX =[\s\S]*?;/;
+const RULE_PATTERN = /([^\r\n]*):\s*{/g;
 
 // =====================================================================================================================
 //  P U B L I C
@@ -11,13 +15,33 @@
 const EsbuildEmotionPlugin = {
     name: 'EsbuildEmotionPlugin',
     setup(build) {
-        console.log('setup', build);
+        const isDev = build.initialOptions.sourcemap;
+        if (isDev) {
+            build.onLoad({filter: /\.jsx$/}, async (args) => {
+                const code = await fs.promises.readFile(args.path, 'utf8');
+                const sxMatch = code.match(SX_PATTERN);
+                if (sxMatch) {
+                    const sxWithLabels = sxMatch[0].replace(RULE_PATTERN, addLabel);
+                    return {
+                        contents: code.replace(SX_PATTERN, sxWithLabels),
+                        loader: 'jsx',
+                    };
+                }
+            });
+        }
     },
 };
 
 // =====================================================================================================================
 //  P R I V A T E
 // =====================================================================================================================
+/**
+ *
+ */
+const addLabel = (found, ruleName) => {
+    const label = ruleName.replace(/[^a-zA-Z0-9]/g, '');
+    return `${ruleName}:{label:'${label}',`;
+};
 
 // =====================================================================================================================
 //  E X P O R T
