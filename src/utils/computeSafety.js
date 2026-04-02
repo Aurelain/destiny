f58305358082dd38daf1fe48bcaa6f6dff5f9d10
aurelain <aurelain@gmail.com>
2026-04-02 09:55:17 +0300

    attempted (4) to fix iphone island overlap

--- src/utils/computeSafety.js
+++ src/utils/computeSafety.js
@@ -2,33 +2,32 @@
  *
  */
 const computeSafety = () => {
-    const isApple = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-    if (isApple) {
-        return {
-            top: 48, // Sufficient for most Dynamic Islands/Notches
-            bottom: 32, // Sufficient for the iOS Home Indicator
-        };
-    }
-
+    // Create hidden element:
     const div = document.createElement('div');
+    div.style.position = 'fixed';
+    div.style.visibility = 'hidden';
+    div.style.pointerEvents = 'none';
 
-    // Set properties so we can measure both top and bottom
+    // Set properties so we can measure both top and bottom:
     div.style.paddingTop = 'env(safe-area-inset-top)';
     div.style.paddingBottom = 'env(safe-area-inset-bottom)';
 
-    div.style.position = 'fixed';
-    div.style.visibility = 'hidden';
-    div.style.pointerEvents = 'none'; // Ensure it doesn't block clicks while briefly in DOM
-
+    // Read properties:
     document.body.appendChild(div);
-
     const computedStyle = getComputedStyle(div);
     const insets = {
         top: parseInt(computedStyle.paddingTop) || 0,
         bottom: parseInt(computedStyle.paddingBottom) || 0,
     };
-
     document.body.removeChild(div);
+
+    // Account for iPhone issues:
+    if (/iPhone|iPad/i.test(navigator.userAgent) && !insets.top) {
+        return {
+            top: 48, // Sufficient for most Dynamic Islands/Notches
+            bottom: 32, // Sufficient for the iOS Home Indicator
+        };
+    }
 
     return insets;
 };
